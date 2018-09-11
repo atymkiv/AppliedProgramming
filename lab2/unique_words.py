798139ae4be9c6bb381e54732da6345f14602822
atyma <atymkiv99@gmail.com>
2018-09-11 20:28:42 +0300

    Created xml file using ElementTree API and counted encounters of every word

--- lab2/unique_words.py
+++ lab2/unique_words.py
@@ -1,11 +1,25 @@
 #Завдання 3. Створити файл c.xml (згідно з стандартами), записати в нього
 #всі унікальні слова та кількість їх вживання у тексті, використовуючи файл а.txt
 #із завдання 2.
-import sys
+import xml.etree.cElementTree as ET
 
-f =  open("a.txt", "r") 
-c = open("c.xml", "w")
-all_words = []
-for i in f:
-	all_words.append(i.split(" "))
-print(all_words)
+root = ET.Element("root")
+doc = ET.SubElement(root,"doc")
+
+f = open('a.txt', "r") 
+
+all_words = dict()
+for line in f:
+	words = line.split()
+	for word in words:
+		if word not in list(all_words):
+			all_words[word] = 1
+		else:
+			all_words[word] = +1
+n = 0;
+for x in all_words:
+	ET.SubElement(doc, "field%s"%n, name = x).text = str(all_words.get(x))
+	n=n+1;
+tree = ET.ElementTree(root)
+tree.write("c.xml")	
+f.close()

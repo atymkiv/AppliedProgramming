4c3b39203e1b1dd10973a08c2a596087b58ef189
atyma <atymkiv99@gmail.com>
2018-10-09 21:08:56 +0300

    starting from template

--- lab3/app.py
+++ lab3/app.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Shell
-from flask_migrate import Migrate, MigrateCommand
+
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -89,5 +89,3 @@ def make_shell_context():
 	return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context = make_shell_context))
 
-migrate = Migrate(app,db)
-manager.add_command('db', MigrateCommand)

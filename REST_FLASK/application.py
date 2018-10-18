5d99b099c2b2ed505816cb5561c7a55d8032b440
atyma <atymkiv99@gmail.com>
2018-10-18 22:59:55 +0300

    Finished database table

--- REST_FLASK/application.py
+++ REST_FLASK/application.py
@@ -10,5 +10,16 @@ db = SQLAlchemy(app)
 class Test(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 
+class Student(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	firstName = db.Column(db.String(30),unique = False)
+	midName = db.Column(db.String(30), unique = False)
+	lastName =  db.Column(db.String(30), unique = False)
+	rate =  db.Column(db.Integer, unique=False)
+
+	def __repr__(self):
+		return '<Прізвище %r>' % self.lastName
+
+
 if __name__ == '__main__':
 	app.run()

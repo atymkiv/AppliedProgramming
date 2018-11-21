fec49146e13fcbd9df7e9e3c826c85dacd2d0d74
atyma <atymkiv99@gmail.com>
2018-11-21 21:56:43 +0200

    finished task3 - image processing using matplotlib and numpy

--- lab5/task3.py
+++ lab5/task3.py
@@ -12,8 +12,22 @@ imgplot = plt.imshow(img)
 lum_img = img[:,:,0]
 plt.imshow(lum_img)
 
+
+
+
+fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(15,5))
+
+for c, ax in zip(range(3), axs):
+    tmp_im = np.zeros(img.shape, dtype="uint8")
+    tmp_im[:,:,c] = img[:,:,c]
+    ax.imshow(tmp_im)
+    ax.set_axis_off()
+plt.show()
+
+
+
 # Making image in black and white
-plt.imshow(lum_img, cmap="gray")
+plt.imshow(lum_img, cmap="gray", vmin =20, vmax=100)
 
 plt.hist(lum_img.ravel(), bins=256, range=(0.0, 1.0), fc='k', ec='k')
 

0809ea9a583cf80d4f05a1c9b0c3719aa02341a3
atyma <atymkiv99@gmail.com>
2018-11-28 13:52:24 +0200

    solved issue with comitting

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -20,12 +20,42 @@ dataset[['Agency', 'Business Title', 'Work Location']]
 
 # Task 3 
 # Вивести назви агентств та кількість вакансій, які вони подали. 
-AgVac = dataset.iloc[:,[1,3]].values
+AgVac = dataset[['Agency', '# Of Positions']]
+AgVac = AgVac.groupby('Agency', sort = False).sum()
+
 
 # Використовуючи бібліотеку Matplotlib відобразити отримані дані у вигляді стовпцеві діаграми. 
-index = np.arange(len(AgVac[:,0]))
-plt.bar(index,AgVac[:,1])
-plt.xlabel('Name of Agency', fontsize=5)
-plt.ylabel('No of vacations', fontsize=5)
-plt.xticks(index, AgVac[:,0], fontsize=5, rotation=30)
-plt.show()
+AgVac[:20].plot(kind = 'bar')
+
+# Task 4
+# Відобразити графіки залежності середнього значення 
+# зарплати від локації та категорії роботи. 
+
+#Залежність середнього значення зарплати від локації
+Location_Salary =  dataset[['Work Location', 'Salary Range To']]
+Location_Salary.columns = ['Work Location', 'Mean Salary']
+Salary = (dataset['Salary Range To']+dataset['Salary Range From'])/2
+Location_Salary.loc[:]['Mean Salary'] = Salary[:]
+
+# Групуєм однакові локації
+Location_Salary = Location_Salary.groupby('Work Location', sort = False).sum()
+
+# Середня зарплата кожної локаці
+Location_Salary.loc[:]['Mean Salary'] = Location_Salary['Mean Salary']/dataset['Work Location'].value_counts(sort=False)
+
+# Виводимо графік
+Location_Salary[:20].plot(kind='bar')
+
+# Залежність середнього значення зарплати від категорії роботи
+Category_Salary = dataset[['Job Category', 'Salary Range To']]
+Category_Salary.columns = ['Job Category', 'Mean Salary']
+Category_Salary.loc[:]['Mean Salary']= Salary[:]
+
+# Групуєм однакові категорії
+Category_Salary = Category_Salary.groupby('Job Category', sort = False).sum()
+
+# Середня зарплата кожної категорії
+Category_Salary.loc[:]['Mean Salary'] = Category_Salary['Mean Salary']/dataset['Job Category'].value_counts(sort=False)
+
+# Виводимо графік
+Category_Salary[:20].plot(kind='bar')
